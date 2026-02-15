0ed00309b7eb590f73872082e35395eb7b6e1c0c
Swarnaly Roy <swarnalyroy667@gmail.com>
2026-02-15 19:08:07 +0530

    added Functions in C question solution

---
+++ FunctionsinC.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int max_of_four(int a, int b, int c, int d) {
+    int max = a; 
+
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+    if (d > max) {
+        max = d;
+    }
+    
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int res = max_of_four(a, b, c, d);
+    printf("%d", res);
+    
+    return 0;
+}
